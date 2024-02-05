0964e305775a5341ca1c35666191b94460726e73
vansh Dhumal <158541510+Vansh24endl@users.noreply.github.com>
2024-02-05 08:29:29 +0530

    Create Day3csi.cpp

---
+++ Day3csi.cpp
@@ -0,0 +1,11 @@
+#include<iosteram>
+user namespace std;
+int main () {
+    int a;
+    cout<< "Enter any natural number:";
+    cin>> a;
+    (a%2==0)? cout<<a<<" is even " : cout<< a<< "is odd " ;
+
+return 0; 
+
+}
