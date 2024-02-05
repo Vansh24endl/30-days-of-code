782daa7fc33c6941b56fc181ca550f236a177601
vansh Dhumal <158541510+Vansh24endl@users.noreply.github.com>
2024-02-05 07:24:32 +0530

    Create Day2csi.cpp

---
+++ Day2csi.cpp
@@ -0,0 +1,15 @@
+#include<iostearm>
+user namespace std;
+
+int main () {
+string name ;
+int age ;
+cout<<"Enter your name";
+cin>> name;
+ cout<<"Enter your age ";
+ cin>>age ;
+cout<<"Hello, "<<name<<" you are "<<age<<" years old";
+
+return 0;
+
+}
