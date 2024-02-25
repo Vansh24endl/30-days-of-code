950281d724d1b064f258c0e7decd4f4e06f62c44
vansh Dhumal <158541510+Vansh24endl@users.noreply.github.com>
2024-02-25 22:26:23 +0530

    Create Day25csi.cpp

---
+++ Day25csi.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int array_Sum(int arr[], int size, int sum);
+int main()
+{
+    int arr[] = {1, 2, 3, 4, 5,6,7};
+  int size = sizeof(arr) / sizeof(arr[0]); // no. of elements in array
+
+    int sum = 0;
+    cout << "The sum of the elements in the array is " << array_Sum(arr, size, sum) << endl;
+    return 0;
+}
+
+int array_Sum(int arr[], int size, int sum){
+    if (size == 0){
+        return sum;
+    }
+    return array_Sum(arr, size - 1, sum + arr[size - 1]);
+}
