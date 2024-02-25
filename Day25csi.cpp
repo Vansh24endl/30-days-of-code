de60374a2054579828e528d7a0f17fb5d5526120
vansh Dhumal <158541510+Vansh24endl@users.noreply.github.com>
2024-02-25 22:29:09 +0530

    Update Day25csi.cpp

--- Day25csi.cpp
+++ Day25csi.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int array_Sum(int arr[], int size, int sum);
 int main()
 {
-    int arr[] = {1, 2, 3, 4, 5,6,7};
+    int arr[] = {1, 2, 3, 4, 5,6,7,8};
   int size = sizeof(arr) / sizeof(arr[0]); // no. of elements in array
 
     int sum = 0;
